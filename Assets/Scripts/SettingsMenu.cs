50bf96674f56a22b0099c7166a4a71e3e49b30c4
deancohen1234 <dean3712@gmail.com>
2024-08-18 03:33:40 -0600

    Updated Map Scene with lighting and prefabs

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
     public Slider MasterVolumeSlider;
+    public Slider MusicVolumeSlider;
     public Slider MouseSensitivitySlider;
-    public CameraSensitivity CameraSensitivity;
+    public AudioMixer MasterMixer;
 
     public const string MASTER_VOLUME_PREF_KEY = "MasterVolume";
+    public const string MUSIC_VOLUME_PREF_KEY = "MusicVolume";
     public const string MOUSE_SENSITIVITY_PREF_KEY = "MouseSensitivity";
 
     // Start is called before the first frame update
@@ -18,9 +21,18 @@ public class SettingsMenu : MonoBehaviour
         LoadSavedSliderValues();
     }
 
-    public void UpdateVolume(float NewValue)
+    public void UpdateMasterVolume(float NewValue)
     {
+        NewValue = Mathf.Max(NewValue, 0.001f);
         PlayerPrefs.SetFloat(MASTER_VOLUME_PREF_KEY, NewValue);
+        MasterMixer.SetFloat("MasterVolume", Mathf.Log10(NewValue) * 20f);
+    }
+
+    public void UpdateMusicVolume(float NewValue)
+    {
+        NewValue = Mathf.Max(NewValue, 0.001f);
+        PlayerPrefs.SetFloat(MUSIC_VOLUME_PREF_KEY, NewValue);
+        MasterMixer.SetFloat("MusicVolume", Mathf.Log10(NewValue) * 20f);
     }
 
     public void UpdateSensitivity(float NewValue)
@@ -28,11 +40,22 @@ public class SettingsMenu : MonoBehaviour
         PlayerPrefs.SetFloat(MOUSE_SENSITIVITY_PREF_KEY, NewValue);
     }
 
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
     private void LoadSavedSliderValues()
     {
         if (PlayerPrefs.HasKey(MASTER_VOLUME_PREF_KEY))
         {
+            Debug.Log("Setting Master: " + PlayerPrefs.GetFloat(MASTER_VOLUME_PREF_KEY));
             MasterVolumeSlider.value = PlayerPrefs.GetFloat(MASTER_VOLUME_PREF_KEY);
+        }
+
+        if (PlayerPrefs.HasKey(MUSIC_VOLUME_PREF_KEY))
+        {
+            MusicVolumeSlider.value = PlayerPrefs.GetFloat(MUSIC_VOLUME_PREF_KEY);
         }
 
         if (PlayerPrefs.HasKey(MOUSE_SENSITIVITY_PREF_KEY))
